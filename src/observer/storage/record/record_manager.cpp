8ca837065d3201418bce84aa5561da4c4186f0b3
AcKing-Sam <295329251@qq.com>
2024-07-15 20:30:39 +0800

    test lab1 18th

--- src/observer/storage/record/record_manager.cpp
+++ src/observer/storage/record/record_manager.cpp
@@ -554,7 +554,7 @@ RC PaxRecordPageHandler::get_chunk(Chunk &chunk)
       int next_off = 0;
       for(int j = 0;j < page_header_->record_num;j ++) {
         if(bitmap.get_bit(j)) {
-          col->append_one(frame_->data() + page_header_->data_offset + column_index[idx - 1] + j * col_len);
+          col->append_one(frame_->data() + page_header_->data_offset + column_index[idx - 1] + next_off);
           next_off += col_len;
         }
       }
